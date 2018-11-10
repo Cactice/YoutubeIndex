020e77dba399ffb0a088ffa1d3670801e0f0128a
Cactice <14835424+Cactice@users.noreply.github.com>
2018-11-11 00:47:20 +0900

    change the look of the web site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+import logo from './YouTube.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <img src={logo}
+          style={{width:300}}/>
+          <p style = {{marginTop:-40}}>
+            Enter URL
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div style = {{marginTop:20}}>
+          <input type="text" />
+            <Button bsStyle="success" style = {{marginLeft:20}}>
+              Analyze URL
+            </Button>
+          </div>
         </header>
       </div>
     );
