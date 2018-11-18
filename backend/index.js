6c8f39e5dcfd00aaf31e13fab1bc5378744a3b43
Cactice <14835424+Cactice@users.noreply.github.com>
2018-11-18 11:02:12 +0900

    add dynamic port support

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
-const port = 3000
-
+const app         = express()
+const port        = 3000
+const dynamicPort = process.env.PORT
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(dynamicPort || port, () => console.log(`Example app listening on port ${port}!`))
