9ce273dc16a15423d1b303c8c3451cc0f09dbac7
Cactice <14835424+Cactice@users.noreply.github.com>
2018-11-21 15:33:44 +0900

    prepare for making a Get request on the front

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import logo from '../assets/YouTube.svg';
 import './App.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
+
 class App extends Component {
+
+    constructor (){
+        super()
+        this.state = {
+            videoId: ''
+        }
+    }
+
+    handleClick = () => {
+        console.log(this.state.videoId)
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo}
-          style={{width:300}}/>
+              style={{width:300}}
+          />
           <p style = {{marginTop:-40}}>
             Enter URL
           </p>
           <div style = {{marginTop:20}}>
-          <input type="text" />
-            <Button bsStyle="success" style = {{marginLeft:20}}>
+          <input type="text" 
+            onChange={(input) => this.setState({videoId: input.target.value})}
+          />
+            <Button 
+              bsStyle="success"
+              style = {{marginLeft:20}} 
+              onClick = {this.handleClick}
+            >
               Analyze URL
             </Button>
           </div>
         </header>
       </div>
+
     );
   }
 }
