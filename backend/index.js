05aa9883ec59717751a9e7a62426171682cf6ef8
Cactice <14835424+Cactice@users.noreply.github.com>
2018-11-23 18:51:27 +0900

    remove unnecessary code

--- backend/index.js
+++ backend/index.js
@@ -12,27 +12,6 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/url/:videoId',renderTranscript)
 
-/*
-    let videoId = req.params.videoId
-    let fetchTranscript = youtubeTranscript(videoId)
-
-    fetchTranscript.then((transcript) => {
-        res.send(JSON.stringify(transcript));
-    })   
-    */
-
-
-
 app.get('/summarize/:text/:language',renderSummary)
-
-/*
-    let text = req.params.text
-    let language = req.params.language
-
-    let fetchSummary = summarize(text,language)
-    fetchSummary.then((summary) => {
-        res.send(JSON.stringify(summary));
-    })   
-*/
 
 app.listen(dynamicPort || port, () => console.log(`Example app listening on port ${port}!`))
